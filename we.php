64563b27c5b0d84b5ae95823a8d616f47a43b83d
Mohammad Moulali <116294232+MohammadMoulali@users.noreply.github.com>
2023-04-30 19:33:50 +0530

    Add files via upload

---
+++ we.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+    <h1 align="center">thanks for submitting your data</h1>
+    <style>
+        h1{
+            border:3 green solid;
+            height:100px;
+            width:auto;
+            background-color: aquamarine;
+        }
+    </style>
+</body>
